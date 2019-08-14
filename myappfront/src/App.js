b8a70d17d182efa9facb5de40d37f7fe984c84f4
GERMAN <GERMANALEXIS231997.GAAC@GMAIL.COM>
2019-08-13 23:43:16 -0600

    utilies

--- myappfront/src/App.js
+++ myappfront/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Home  from './Components/Pages/Public/Home/Home';
+import Login from './Components/Pages/Public/Login/Login';
+//import sigin from './Components/Pages/Public/sigin/sigin';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+
+      <Login/>
+    </section>
   );
 }
 
